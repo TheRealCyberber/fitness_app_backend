260562f1e238981bd9b5eea9b82251fbf1871759
TheRealCyberber <shafi.albalushi@gmail.com>
2025-06-25 22:35:21 +0300

    changed name

--- routes/AuthRouter.js
+++ routes/AuthRouter.js
@@ -1,5 +1,5 @@
 const router = require('express').Router()
-const controller = require('../controllers/AuthController')
+const controller = require('../controllers/authControllers')
 
 router.post('/login', controller.Login)
 router.post('/register', controller.Register)
