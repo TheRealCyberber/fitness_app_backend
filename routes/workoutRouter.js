9a36663c98b3d1781e713b11a958a9509b4e91ea
Shahla <shahlaa.ah@gmail.com>
2025-06-26 17:47:16 +0300

    Update workoutRouter.js

--- routes/workoutRouter.js
+++ routes/workoutRouter.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const router = express.Router()
-const workoutController = require('../controllers/WorkoutController.js')
+const workoutController = require('../controllers/workoutController.js')
 const middleware = require('../middleware')
 
 
