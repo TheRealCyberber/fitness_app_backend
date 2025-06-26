37025bc6dc5b135db37a8daf352e6d6ba854c141
Shahla <shahlaa.ah@gmail.com>
2025-06-26 17:46:57 +0300

    Update progressRouter.js

--- routes/progressRouter.js
+++ routes/progressRouter.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const router = express.Router()
-const progressController = require('../controllers/ProgressController.js')
+const progressController = require('../controllers/progressController.js')
 const middleware = require('../middleware')
 
 
