fc3d2c34a4f9db3befdfe2ee830b3a888b3f6ccf
TheRealCyberber <shafi.albalushi@gmail.com>
2025-06-29 13:16:17 +0300

    rectified workout add functionality and displaying the workouts

--- models/Workout.js
+++ models/Workout.js
@@ -2,6 +2,7 @@ const mongoose = require('mongoose');
 
 const workoutSchema = new mongoose.Schema({
   userId: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
+  name: {type: String, required: true },
   type: { type: String, required: true },
   duration: { type: Number, required: true }, 
   calories: { type: Number, required: true }, 
